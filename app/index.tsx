330cdbc49f19bdc379c91571478fc4bbdce1a8e9
padsue <your-email@example.com>
2025-08-12 22:49:43 +0800

    Minor Adjustments

--- app/index.tsx
+++ app/index.tsx
@@ -1,51 +1,48 @@
 import React from "react";
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, Pressable,} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LandingPage() {
     const router = useRouter();
 
     return (
-        <View className="flex min-h-screen flex-col items-center justify-center bg-white p-4 text-center">
-        <View className="flex flex-col items-center justify-center flex-grow space-y-8">
-            <Image
-            source={require("../assets/logo.png")}
-            className="w-32 h-32"
-            resizeMode="contain"
-            />
-            <View className="space-y-2 items-center">
-            <Text className="text-4xl font-bold text-[#C43670]">Derm</Text>
-            <Text className="text-4xl font-bold text-[#f2838f]">AI</Text>
-            <Text className="text-gray-500 text-sm">Smarter Skin Starts Here</Text>
+        <SafeAreaView>
+            <View className="flex min-h-screen flex-col items-center justify-center bg-white p-4 text-center">
+            <View className="flex flex-col items-center justify-center flex-grow space-y-8">
+                <Image
+                source={require("../assets/logo.png")}
+                className="w-36 h-36"
+                resizeMode="contain"
+                />
+                <View className="items-center">
+                <Text className="text-4xl font-bold">
+                    <Text className="text-[#C43670]">Derm</Text>
+                    <Text className="text-[#f2838f]">AI</Text>
+                </Text>
+                <Text className="text-gray-500 text-md">Smarter Skin Starts Here</Text>
+                </View>
             </View>
-        </View>
 
-        <View className="w-full max-w-sm space-y-4 mb-8">
-            <Pressable onPress={() => router.push("/sign-up")}>
-            <LinearGradient
-                colors={["#f3cc97", "#C43670"]}
-                className="w-full h-12 rounded-full flex items-center justify-center"
-            >
-                <Text className="text-white font-semibold">Let's get started</Text>
-            </LinearGradient>
-            </Pressable>
+            <View className="w-full max-w-sm mb-20">
 
-            <Pressable
-            onPress={() => router.push("/sign-in")}
-            className="w-full h-12 rounded-full bg-white flex items-center justify-center shadow"
-            >
-            <Text className="text-[#C43670] font-semibold">
-                I already have an account
-            </Text>
-            </Pressable>
-        </View>
+                <Pressable onPress={() => router.push("/sign-up")}
+                 className= "w-full h-12 flex items-center bg-[#C43670] justify-center shadow-lg shadow-black mb-4 rounded-lg">
+                    <Text className="text-white font-semibold">Let's get started</Text>
+                </Pressable>
 
-        <Text className="text-[10px] text-gray-500 text-center">
-            By continuing, you agree to DermAI's{" "}
-            <Text className="text-pink-500 underline">Terms of Service</Text> and{" "}
-            <Text className="text-pink-500 underline">Privacy Policy</Text>
-        </Text>
-        </View>
+                <Pressable
+                onPress={() => router.push("/sign-in")}
+                className="w-full h-12 bg-white flex items-center justify-center shadow-lg shadow-black mb-10 rounded-lg"
+                >
+                <Text className="text-[#C43670] font-semibold">
+                    I already have an account
+                </Text>
+                </Pressable>
+            </View>
+
+            </View>
+        </SafeAreaView>
     );
 }
