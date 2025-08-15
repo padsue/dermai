531470276c5b8244552dfbed256eacbb963bdb57
Jaren Quegan <queganjaren@gmail.com>
2025-08-16 02:08:23 +0800

    Create index.ts

---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+// API Services
+export * from './services';
+export * from './services/authService';
+export * from './services/firestoreService';
+
+// Types
+export * from './types';
+
+// Configuration
+export { db, auth } from './config/firebase';
